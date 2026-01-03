19f38ccbde4bb44bcea227dfbb1bccba00d0901c
NKG-09 <200639623+NKG-09@users.noreply.github.com>
2026-01-03 16:23:37 +0530

    Add some extra items

--- src/home.js
+++ src/home.js
@@ -3,10 +3,19 @@ import { Item } from "./item.js";
 export const items = [
   new Item("Alfredo", "Pasta", "$20", ""),
   new Item("Lasagna", "Pasta", "$20", ""),
+  new Item("Spaghetti", "Pasta", "$20", ""),
+  new Item("Fettuccine", "Pasta", "$20", ""),
+  new Item("Trenette", "Pasta", "$20", ""),
   new Item("Roman", "Pizza", "$20", ""),
   new Item("Sicilian", "Pizza", "$20", ""),
+  new Item("Neapolitan", "Pizza", "$20", ""),
+  new Item("Fritta", "Pizza", "$20", ""),
+  new Item("Margherita", "Pizza", "$20", ""),
   new Item("Raspberry", "Tiramisu", "$20", ""),
   new Item("Pumpkin", "Tiramisu", "$20", ""),
+  new Item("Chocolate", "Tiramisu", "$20", ""),
+  new Item("Berry", "Tiramisu", "$20", ""),
+  new Item("Strawberry", "Tiramisu", "$20", ""),
 ];
 
 export function loadHome(content = document.querySelector("#content")) {
